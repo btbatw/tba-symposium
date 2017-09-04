b27dec0810783eb8c3b81731e1d5f9f16a753477
Yu-An Chen <atwood12@gmail.com>
2017-09-04 10:47:47 -0500

    Add typography and theming of angular material

--- client/src/main.ts
+++ client/src/main.ts
@@ -4,6 +4,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+// Gesture Support for angular material
+import { hammerjs } from 'hammerjs';
+
 if (environment.production) {
   enableProdMode();
 }
