59c8a132da986cbdbd26718d56a47234cbc25932
Yu-An Chen <atwood12@gmail.com>
2017-09-06 03:07:41 -0500

    Seperate website-about component

---
+++ client/src/app/website/website-about/website-about.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+	selector: 'app-website-about',
+	templateUrl: './website-about.component.html',
+	styleUrls: ['./website-about.component.scss']
+})
+export class WebsiteAboutComponent {
+	@Input() symposium;
+	constructor() { }
+
+	ngOnInit() {
+	}
+
+}
--- client/src/app/website/website.module.ts
+++ client/src/app/website/website.module.ts
@@ -7,12 +7,14 @@ import { WebsiteRoutingModule } from './website-routing.module';
 import { WebsiteHeaderComponent } from './website-header/website-header.component';
 import { WebsiteTalkComponent } from './website-talk/website-talk.component';
 import { WebsiteTalkSpeakerComponent } from './website-talk/website-talk-speaker/website-talk-speaker.component';
+import { WebsiteAboutComponent } from './website-about/website-about.component';
+import { WebsiteAboutCarouselComponent } from './website-about/website-about-carousel/website-about-carousel.component';
 
 
 
 @NgModule({
   imports: [ SharedModule, WebsiteRoutingModule ],
-  declarations: [ WebsiteComponent, WebsiteHeaderComponent, WebsiteTalkComponent, WebsiteTalkSpeakerComponent ],
+  declarations: [ WebsiteComponent, WebsiteHeaderComponent, WebsiteTalkComponent, WebsiteTalkSpeakerComponent, WebsiteAboutComponent, WebsiteAboutCarouselComponent ],
   providers: [ WebsiteService ]
 })
 export class WebsiteModule { }
