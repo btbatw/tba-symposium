75cb15b8606e3023eb96e12fd03213907c9122dc
Yu-An Chen <atwood12@gmail.com>
2017-09-07 00:48:23 -0500

    Important! Dialog component needs to be added to entryComponents

--- client/src/app/website/website.module.ts
+++ client/src/app/website/website.module.ts
@@ -7,6 +7,7 @@ import { WebsiteRoutingModule } from './website-routing.module';
 import { WebsiteHeaderComponent } from './website-header/website-header.component';
 import { WebsiteTalkComponent } from './website-talk/website-talk.component';
 import { WebsiteTalkSpeakerComponent } from './website-talk/website-talk-speaker/website-talk-speaker.component';
+import { SpeakerDetailDialog } from './website-talk/website-talk-speaker/speaker-detail-dialog/speaker-detail-dialog.component';
 import { WebsiteAboutComponent } from './website-about/website-about.component';
 import { WebsiteAboutCarouselComponent } from './website-about/website-about-carousel/website-about-carousel.component';
 
@@ -14,7 +15,8 @@ import { WebsiteAboutCarouselComponent } from './website-about/website-about-car
 
 @NgModule({
   imports: [ SharedModule, WebsiteRoutingModule ],
-  declarations: [ WebsiteComponent, WebsiteHeaderComponent, WebsiteTalkComponent, WebsiteTalkSpeakerComponent, WebsiteAboutComponent, WebsiteAboutCarouselComponent ],
-  providers: [ WebsiteService ]
+  declarations: [ WebsiteComponent, WebsiteHeaderComponent, WebsiteTalkComponent, WebsiteTalkSpeakerComponent, SpeakerDetailDialog, WebsiteAboutComponent, WebsiteAboutCarouselComponent ],
+  providers: [ WebsiteService ],
+  entryComponents: [ SpeakerDetailDialog ]
 })
 export class WebsiteModule { }
