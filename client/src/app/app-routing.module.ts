9113720b15015a68a81e308d10fbf78552916d24
Yu-An Chen <atwood12@gmail.com>
2017-09-08 01:48:59 -0500

    Fix routing issues with github pages

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 export const routes: Routes = [
 	{ path: '', redirectTo: 'website', pathMatch: 'full' },
+	{ path: '**', redirectTo: 'website', pathMatch: 'full' }
+	// http://www.backalleycoder.com/2016/05/13/sghpa-the-single-page-app-hack-for-github-pages/
 	// { path: 'crisis', loadChildren: 'app/crisis/crisis.module#CrisisModule' },
 	// { path: 'heroes', loadChildren: 'app/hero/hero.module#HeroModule' }
 ];
