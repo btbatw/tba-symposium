f09a83ff139a993f517aaeebf13e21990b901b5e
Yu-An Chen <atwood12@gmail.com>
2017-09-08 01:47:18 -0500

    Add polyfills for browser support

--- client/src/main.ts
+++ client/src/main.ts
@@ -4,9 +4,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-// Gesture Support for angular material
-import { hammerjs } from 'hammerjs';
-
 if (environment.production) {
   enableProdMode();
 }
