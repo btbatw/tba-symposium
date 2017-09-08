1b720298d8c3c04974b394de01edfb8a2ab40bf5
Yu-An Chen <atwood12@gmail.com>
2017-09-08 01:54:19 -0500

    Sort to show keynote speakers on top

--- client/src/app/website/website.service.ts
+++ client/src/app/website/website.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 // import { Http, Response } from '@angular/http';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 
 import { Observable } from 'rxjs/Observable';
@@ -19,7 +19,7 @@ export class WebsiteService {
 	getSymposium() {
 		// return this.http
 		// 	.get('api/symposium/59a768dec853bd1942bd5e71')
-			// .map(this.extractData)
+		// 	.map(this.extractData)
 		return fromPromise(Promise.resolve([ttbaSymposium2017]))
 			.map(data => {
 				this.speakers = this.parseSpeakers(data[0]);
@@ -27,6 +27,21 @@ export class WebsiteService {
 			});
 	}
 
+	// getSymposiumFromGithub() {
+	// 	const test = require('https://github.com/Yu-AnChen/tba-symposium/blob/master/client/src/app/website/symposium-ttba-2017.ts');
+	// 	console.log(test);
+	// let headers = new HttpHeaders().set('accept', 'application/vnd.github.VERSION.raw');
+	// console.log(headers);
+	// return this.http
+	// 	.get('https://api.github.com/repos/yu-anchen/tba-symposium/contents/client/src/app/website/symposium-ttba-2017.ts',
+	// 		{
+	// 			headers: new HttpHeaders().set('accept', 'application/vnd.github.VERSION.raw'),
+	// 			observe: 'response',
+	// 			responseType: 'text'
+	// 		}
+	// 	)
+	// 	.subscribe(data => console.log(data));
+	// }
 	private parseSpeakers(data) {
 		return data.talk.schedules
 			.filter(element => element.speakers[0])
@@ -35,10 +50,16 @@ export class WebsiteService {
 				let schedule = { ...element };
 				delete schedule.speakers;
 				return element.speakers.map(element =>
-					element = { ...element, ...schedule}
+					element = { ...element, ...schedule }
 				);
 			})
-			.reduce((prev, curr) => [...prev, ...curr], []);
+			.reduce((prev, curr) => [...prev, ...curr], [])
+			.sort((a, b) => {
+				let titleA = a.title.split(':')[0];
+				let titleB = b.title.split(':')[0];
+				return titleA === titleB ? 0 :
+					titleA > titleB ? 1 : -1;
+			});
 	}
 
 	private extractData(res: Response): any {
