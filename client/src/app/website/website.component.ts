cba86b716b278c01f7817d127907a298f95ad1d4
Yu-An Chen <atwood12@gmail.com>
2017-09-10 00:06:13 -0500

    Implement UI for dynamic theming

--- client/src/app/website/website.component.ts
+++ client/src/app/website/website.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { WebsiteService } from './website.service';
 
+import { OverlayContainer } from '@angular/material';
+
 @Component({
 	selector: 'app-website',
 	templateUrl: './website.component.html',
@@ -11,13 +13,24 @@ import { WebsiteService } from './website.service';
 export class WebsiteComponent implements OnInit {
 	symposium;
 	speakers;
-	constructor(private websiteService: WebsiteService) { }
+
+	private themeBright = 'amber-blue-theme quicksand-typography';
+	private themeTechie = 'white-light-blue-theme roboto-typography';
+
+	websiteTheme = this.themeBright;
+
+	constructor(private websiteService: WebsiteService, private overlayContainer: OverlayContainer) { }
 
 	ngOnInit() {
+		this.overlayContainer.themeClass = this.websiteTheme.split(' ')[0];
 		this.websiteService.getSymposium()
 			.subscribe(result => {
 				this.symposium = result[0];
 				this.speakers = this.websiteService.speakers;
 			});
+	}
+	test() {
+		this.websiteTheme = this.websiteTheme === this.themeBright ? this.themeTechie : this.themeBright;
+		this.overlayContainer.themeClass = this.websiteTheme.split(' ')[0];
 	}
 }
