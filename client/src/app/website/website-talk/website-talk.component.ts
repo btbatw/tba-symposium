eabc0564967bcecdc0da4b8e255cb73deaf6829b
Yu-An Chen <atwood12@gmail.com>
2017-09-15 22:47:40 -0500

    Add summary-detail animation

--- client/src/app/website/website-talk/website-talk.component.ts
+++ client/src/app/website/website-talk/website-talk.component.ts
@@ -1,10 +1,41 @@
 import { Component, Input } from '@angular/core';
 import * as moment from 'moment-timezone';
 
+import {
+	trigger,
+	style,
+	animate,
+	transition,
+	group,
+	state
+} from '@angular/animations';
+
 @Component({
 	selector: 'app-website-talk',
 	templateUrl: './website-talk.component.html',
-	styleUrls: ['./website-talk.component.scss']
+	styleUrls: ['./website-talk.component.scss'],
+	animations: [
+		trigger('itemAnim', [
+			transition(':enter', [
+				style({ height: 0 }),
+				animate('0.1s ease-out', style({ height: '*' }))
+			]),
+			transition(':leave', [
+				style({ height: '*' }),
+				animate('0.1s ease-in', style({ height: 0 }))
+			])
+		]),
+		trigger('focusPanel', [
+			state('inactive', style({
+				transform: 'scale(1)',
+			})),
+			state('active', style({
+				transform: 'rotate(90deg)',
+			})),
+			transition('inactive => active', animate('100ms ease-out')),
+			transition('active => inactive', animate('100ms ease-in'))
+		])
+	]
 })
 export class WebsiteTalkComponent {
 	@Input() speakers;
@@ -29,7 +60,6 @@ export class WebsiteTalkComponent {
 		this.showing.includes(title) ?
 			this.showing = this.showing.filter(element => element !== title) :
 			this.showing = [title, ...this.showing];
-		console.log(this.showing);
 	}
 
 }
