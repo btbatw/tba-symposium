f70dd93ce4595aec681bd0ef30797500b912c805
Unknown <atwood12@gmail.com>
2020-06-06 17:46:22 -0400

    Clean up routing, since we only have on page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WebsiteComponent } from './website/website.component';
 
 export const routes: Routes = [
 	{ path: 'admin', loadChildren: 'app/admin/admin.module#AdminModule' },
-	{ path: '', redirectTo: 'website', pathMatch: 'full' },
-	{ path: '**', redirectTo: 'website', pathMatch: 'full' }
+	{ path: '', component: WebsiteComponent},
+	{ path: '**', redirectTo: '', pathMatch: 'full' },
+	// { path: '', redirectTo: 'website', pathMatch: 'full' },
 	// http://www.backalleycoder.com/2016/05/13/sghpa-the-single-page-app-hack-for-github-pages/
 	// { path: 'crisis', loadChildren: 'app/crisis/crisis.module#CrisisModule' },
 	// { path: 'heroes', loadChildren: 'app/hero/hero.module#HeroModule' }
